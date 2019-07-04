5d3b2d82111dc5de752162eba3a8540381219dc3
Filip <you@example.com>
2019-07-04 18:33:55 +0200

    redux apply resolved

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import servicesReducer from './servicesReducer';
 import languageReducer from './languageReducer';
 
-
 export default combineReducers({
   services: servicesReducer,
   language: languageReducer
